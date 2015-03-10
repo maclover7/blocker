2e5d58a7d8db9e0aed0c82fe8c234bc8142ba232
Jon Moss <me@jonathanmoss.me>
2015-03-10 19:52:33 -0400

    it works! home page and omniauth controller/model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+
+  # Omniauth stuff
+  def self.from_omniauth(auth, signed_in_resource=nil)
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+    unless user
+      user = User.create(
+        provider: auth.provider,
+        uid:      auth.uid,
+        name:     auth.info.name)
+      end
+    user
+  end
 end
