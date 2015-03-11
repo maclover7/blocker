182f41a507008baee298d055d13fd9bc96ce617d
Jon Moss <me@jonathanmoss.me>
2015-03-10 20:36:00 -0400

    admin backend

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,19 @@
 class PagesController < ApplicationController
+  before_filter :authenticate, only: [:admin]
+
   def home
     @user_count = User.count
+  end
+
+  def admin
+    @user_count = User.count
+    @users = User.all
+  end
+
+  protected
+    def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['username'] && password == ENV['password']
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
 
+  get 'admin' => "pages#admin"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
